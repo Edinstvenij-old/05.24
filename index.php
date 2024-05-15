9092ede8a5d33fbb229dad6c226ac0a5979a04b0
Nicolay Vig <nikvigovskij@gmail.com>
2024-05-16 00:15:10 +0300

    test

--- index.php
+++ index.php
@@ -2,9 +2,3 @@
 
 ///require_once __DIR__ . '/scripts/function.php';
 ///require_once __DIR__ . '/info.php';
-
-require_once 'velueObject.php';
-
-$color = RGBColor::random();
-
-echo "<div style='width: 100px; height: 100px; background-color: {$color};'></div>";
